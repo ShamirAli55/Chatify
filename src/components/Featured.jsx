235c551ce7d49a7f7d0ac62763fb4b572d3709c9
ShamirAli55 <shamirali9779@gmail.com>
2025-09-13 23:51:39 +0500

    routes configured

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -4,15 +4,19 @@ import React from 'react'
 const Featured = () => {
   return (
     <div>
-      <h1><b>Hey there !</b> Discover my stories and creative ideas.</h1>
-      <div>
-        <div className='relative w-[200px] h-[200px]'>
-          <Image src={"/p1.jpeg"} alt='' fill/>
+      <h1 className='text-7xl'><b>Hey there !</b> Discover my stories and creative ideas.</h1>
+
+      <div className='flex gap-5 mt-10 w-full min-h-[400px] pt-6 justify-between'>
+        <div className='relative w-1/2'>
+          <Image src={"/p1.jpeg"} alt='' fill className='object-cover rounded-lg'/>
         </div>
-        <div>
+
+        <div className='p-4 pl-0 w-1/2 h-full flex justify-center items-center gap-4'>
+           <div>
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, totam?</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae impedit ratione, natus magni repudiandae quaerat non, libero mollitia est totam vero ut eligendi facere pariatur excepturi eveniet, quasi repellendus blanditiis.</p>
             <button>Read more</button>
+           </div>
         </div>
       </div>
     </div>
