529abb47424f04a031e308522f610753c7106280
ShamirAli55 <shamirali9779@gmail.com>
2025-09-15 18:01:59 +0500

    token generated

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,43 +1,67 @@
 import User from "../models/User.js";
-export const signup = async (req, res) => {
-  const { fullName, email, password } = req.body;
+import {generateToken} from "../lib/utils.js";
+import bcrypt from "bcrypt";
+export const signup = async (req, res) => 
+{
+    const { fullName, email, password } = req.body;
 
-  try {
-    if (!fullName || !email || !password) {
-      return res.status(400).json({ message: "All fields are required" });
+    try {
+        if (!fullName || !email || !password) 
+        {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        if (password.length < 6) 
+        {
+            return res
+                .status(400)
+                .json({ message: "Password must be at least 6 characters" });
+        }
+
+        const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+        if (!emailRegex.test(email)) 
+        {
+            return res.status(400).json({ message: "Please enter a valid email" });
+        }
+
+        const user = await User.findOne({ email });
+
+        if (user) 
+        {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({ fullName, email, password: hashedPassword });
+        if (newUser)
+        {
+            generateToken(newUser._id,res);
+            await newUser.save();
+            return res.status(201).json({ 
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic
+            });
+        } 
+        else 
+        {
+            return res.status(500).json({ message: "Internal server error" });
+        }
+    } 
+    catch (error) 
+    {
+        console.error("Error in signup controller :", error);
+        return res.status(500).json({ message: "Internal server error" });
     }
-
-    if (password.length < 6) {
-      return res
-        .status(400)
-        .json({ message: "Password must be at least 6 characters" });
-    }
-
-    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-
-    if (!emailRegex.test(email)) {
-      return res.status(400).json({ message: "Please enter a valid email" });
-    }
-
-    const user = await User.findOne({ email });
-
-    if (user) {
-      return res.status(400).json({ message: "User already exists" });
-    } else {
-      const newUser = new User({ fullName, email, password });
-      await newUser.save();
-      return res.status(201).json({ message: "User created successfully" });
-    }
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Internal server error" });
-  }
 };
 
 export const login = (req, res) => {
-  res.send("Login");
+    res.send("Login");
 };
 
 export const logout = (req, res) => {
-  res.send("Logout");
+    res.send("Logout");
 };
