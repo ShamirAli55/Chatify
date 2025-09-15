c62d0504e86e86e38bf91afae7e9fd305dfbb9c8
ShamirAli55 <shamirali9779@gmail.com>
2025-09-15 18:21:02 +0500

    signup section code refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -30,16 +30,19 @@ export const signup = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new User({ fullName, email, password: hashedPassword });
-    if (newUser) {
-      generateToken(newUser._id, res);
-      await newUser.save();
+    if (newUser) 
+    {
+     const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
       return res.status(201).json({
         _id: newUser._id,
         fullName: newUser.fullName,
         email: newUser.email,
         profilePic: newUser.profilePic,
       });
-    } else {
+    } 
+    else 
+    {
       return res.status(500).json({ message: "Internal server error" });
     }
   } catch (error) {
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,9 +2,15 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    const { MONGO_URI } = process.env;
+    if (!MONGO_URI) throw new Error("MONGO_URI is not defined");
+
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
+
+  } 
+  catch (error) 
+  {
     console.log("Error in DB connection", error);
     process.exit(1);
   }
