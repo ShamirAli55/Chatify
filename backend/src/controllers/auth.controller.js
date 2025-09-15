abe443d393999de59a6878fdcb8fbf446767cb5e
ShamirAli55 <shamirali9779@gmail.com>
2025-09-15 20:53:40 +0500

    add cloudinary integration and update profile controller for image upload

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import { generateToken } from "../lib/utils.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -79,6 +80,28 @@ export const logout = (_, res) => {
   res.status(200).json({ message: "Logged out successfully" });
 };
 
-export const updateProfile = () => {
-  
+export const updateProfile = async () => {
+  try {
+    const { profilePic } = req.body;
+
+    if (!profilePic)
+      return res.status(400).json({ message: "Profile picture is required" });
+
+    const userId = req.user._id;
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } 
+  catch (error) 
+  {
+    console.error("Error in updateProfile controller :", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
