72b4d2a85aa9ad6ba64ade8b5f374e21513acecc
ShamirAli55 <shamirali9779@gmail.com>
2025-09-15 16:22:50 +0500

    db connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import connectDB from "./lib/db.js";
 
 dotenv.config();
 const app = express();
@@ -13,9 +14,16 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server is running on port : ", PORT);
+  connectDB();
 });
