ffd4f3b60a07ed659b3ff2a9fa0e52fa7e3d6471
ShamirAli55 <shamirali9779@gmail.com>
2025-09-16 09:35:40 +0500

    sign-up implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,13 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path";
+import cors from "cors";
 import connectDB from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
 
 app.use(express.json());
+app.use(cors({origin:process.env.CLIENT_URL,credentials:true}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
-import {Routes, Route} from "react-router";
+import {Routes, Route, Navigate} from "react-router";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import ChatPage from "./pages/ChatPage";
 import {useAuthStore} from "./store/useAuthStore";
+import { useEffect } from "react";
+import PageLoader from "./components/PageLoader";
+import {Toaster} from "react-hot-toast";
 export default function App() 
 {
+  const {checkAuth,isCheckingAuth,authUser} = useAuthStore();
+  useEffect(()=>{
+    checkAuth();
+  },[checkAuth]);
+
+  console.log(checkAuth, isCheckingAuth);
+
+  if(isCheckingAuth) return <PageLoader/>
   return (
     <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
 
@@ -13,10 +24,12 @@ export default function App()
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
 
       <Routes>
-        <Route index path="/" element={<ChatPage />}/>
-        <Route path="/login" element={<LoginPage />}/>
-        <Route path="/signup" element={<SignUpPage />}/>
+        <Route index path="/" element={authUser ?<ChatPage /> :<Navigate to="/login" />}/>
+        <Route path="/login" element={!authUser ?<LoginPage /> :<Navigate to="/" />}/>
+        <Route path="/signup" element={!authUser ?<SignUpPage /> :<Navigate to="/" />}/>
       </Routes> 
+
+      <Toaster/>
     </div>
   )
 }
