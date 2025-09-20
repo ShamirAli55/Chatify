2a3e8d9da9790e6097025ec19e7b388713aae234
ShamirAli55 <shamirali9779@gmail.com>
2025-09-20 13:53:19 +0500

    minor fixes

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -18,17 +18,17 @@ const SignUpPage = () =>
     signup(formData);
   }
   return (
- <div className="w-full flex items-center justify-center p-4 bg-slate-900">
-      <div className="relative w-full max-w-6xl md:h-[800px] h-[650px]">
+ <div className="w-full flex items-center justify-center px-22  bg-slate-900">
+      <div className="relative w-full md:max-h-[90vh] h-[650px]">
         <BorderAnimatedContainer>
           <div className="w-full flex flex-col md:flex-row">
             {/* FORM CLOUMN - LEFT SIDE */}
             <div className="md:w-1/2 p-8 flex items-center justify-center md:border-r border-slate-600/30">
               <div className="w-full max-w-md">
                 {/* HEADING TEXT */}
-                <div className="text-center mb-8">
-                  <MessageCircleIcon className="w-12 h-12 mx-auto text-slate-400 mb-4" />
-                  <h2 className="text-2xl font-bold text-slate-200 mb-2">Create Account</h2>
+                <div className="text-center my-8">
+                  <MessageCircleIcon className="w-12 h-10 mx-auto text-slate-400" />
+                  <h2 className="text-xl font-bold text-slate-200 mb-2">Create Account</h2>
                   <p className="text-slate-400">Sign up for a new account</p>
                 </div>
 
@@ -92,7 +92,7 @@ const SignUpPage = () =>
                   </button>
                 </form>
 
-                <div className="mt-6 text-center">
+                <div className="my-6 text-center">
                   <Link to="/login" className="auth-link">
                     Already have an account? Login
                   </Link>
